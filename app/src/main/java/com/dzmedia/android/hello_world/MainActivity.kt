cbf10b1621db6fece53f5625e0ca0c8b88d21228
Zucker <dzucker@amazon.com>
2018-07-05 09:50:08 -0700

    add app

---
+++ app/src/main/java/com/dzmedia/android/hello_world/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dzmedia.android.hello_world
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
